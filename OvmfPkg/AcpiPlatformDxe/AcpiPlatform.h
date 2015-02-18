25e141a07979dd1ea21610b5dbf01fe1186ba390
Laszlo Ersek <lersek@redhat.com>
2015-02-18 09:49:54 +0100

    OvmfPkg: AcpiPlatformDxe: extract common entry point
    Currently the entry point functions of both driver builds
    (AcpiPlatformDxe.inf and QemuFwCfgAcpiPlatformDxe.inf) directly contain
    the logic that is different between the two builds.
    
    Because we're going to restructure the entry point logic soon, we'd have
    to duplicate the same new code between both entry point functions.
    
    Push down the logic in which they differ to a new function:
    - InstallAcpiTables() [AcpiPlatform.c]
    - InstallAcpiTables() [QemuFwCfgAcpiPlatform.c]
    
    and extract a common entry point function:
    - AcpiPlatformEntryPoint() [EntryPoint.c]
    
    which we can soon modify without code duplication.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>

--- OvmfPkg/AcpiPlatformDxe/AcpiPlatform.h
+++ OvmfPkg/AcpiPlatformDxe/AcpiPlatform.h
@@ -67,5 +67,10 @@ InstallQemuFwCfgTables (
   IN   EFI_ACPI_TABLE_PROTOCOL       *AcpiProtocol
   );
 
+EFI_STATUS
+EFIAPI
+InstallAcpiTables (
+  IN   EFI_ACPI_TABLE_PROTOCOL       *AcpiTable
+  );
 #endif
 
