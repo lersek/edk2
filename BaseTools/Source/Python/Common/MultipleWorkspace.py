lersek/edk2

commit 2b1c08acfceb94326c67b7d8f9fe5d8ab4cb7f61

BaseTools: fix the bug for [BuildOptions] of multiple workspace support
when enable Multiple workspace and there have other option(eg: -I) before
$(WORKSPACE), handleWsMacro cannot return correct which cause the
ArmVirtPkg build failure.
example:
[BuildOptions]
  *_*_AARCH64_PLATFORM_FLAGS == -I$(WORKSPACE)/ArmVirtPkg/Include

Cc: Liming Gao <liming.gao@intel.com>
Contributed-under: TianoCore Contribution Agreement 1.0
Signed-off-by: Yonghong Zhu <yonghong.zhu@intel.com>
Reviewed-by: Liming Gao <liming.gao@intel.com>

BaseTools/Source/Python/Common/MultipleWorkspace.py
@@ -4,7 +4,7 @@
 # This file is required to make Python interpreter treat the directory
 # as containing package.
 #
-# Copyright (c) 2015, Intel Corporation. All rights reserved.<BR>
+# Copyright (c) 2015 - 2016, Intel Corporation. All rights reserved.<BR>
 # This program and the accompanying materials
 # are licensed and made available under the terms and conditions of the BSD License
 # which accompanies this distribution.  The full text of the license may be found at
@@ -128,12 +128,17 @@ class MultipleWorkspace(object):
     @classmethod
     def handleWsMacro(cls, PathStr):
         if TAB_WORKSPACE in PathStr:
-            Path = PathStr.replace(TAB_WORKSPACE, cls.WORKSPACE).strip()
-            if not os.path.exists(Path):
-                for Pkg in cls.PACKAGES_PATH:
-                    Path = PathStr.replace(TAB_WORKSPACE, Pkg).strip()
-                    if os.path.exists(Path):
-                        return Path
+            PathList = PathStr.split()
+            if PathList:
+                for i, str in enumerate(PathList):
+                    if str.find(TAB_WORKSPACE) != -1:
+                        MacroStartPos = str.find(TAB_WORKSPACE)
+                        MacroEndPos = str.find(')', MacroStartPos)
+                        Substr = str[MacroEndPos+1:]
+                        if Substr.startswith('/') or Substr.startswith('\\'):
+                            Substr = Substr[1:]
+                        PathList[i] = str[0:MacroStartPos] + os.path.normpath(cls.join(cls.WORKSPACE, Substr))
+            PathStr = ' '.join(PathList)
         return PathStr
     
     ## getPkgPath()

commit 72305ef8e3a33243f7c71a5b0e0b0f6c9a2ecba6

BaseTools: handleWsMacro: keep trailing os.sep in WORKSPACE substitution
The NASM utility requires a trailing directory separator for its -I and -i
options (this is a documented requirement). The INF file
"OvmfPkg/ResetVector/ResetVector.inf" conforms to it:

  [BuildOptions]
     *_*_IA32_NASMB_FLAGS = -I$(WORKSPACE)/UefiCpuPkg/ResetVector/Vtf0/
     *_*_X64_NASMB_FLAGS = -I$(WORKSPACE)/UefiCpuPkg/ResetVector/Vtf0/

but handleWsMacro currently removes such trailing separators with the
os.path.normpath() function, breaking the OVMF build. Restore the trailing
separator when necessary.

Cc: Yonghong Zhu <yonghong.zhu@intel.com>
Cc: Liming Gao <liming.gao@intel.com>
Fixes: 2b1c08acfceb94326c67b7d8f9fe5d8ab4cb7f61
Contributed-under: TianoCore Contribution Agreement 1.0
Signed-off-by: Laszlo Ersek <lersek@redhat.com>

BaseTools/Source/Python/Common/MultipleWorkspace.py
@@ -137,7 +137,11 @@ class MultipleWorkspace(object):
                         Substr = str[MacroEndPos+1:]
                         if Substr.startswith(os.sep):
                             Substr = Substr[1:]
-                        PathList[i] = str[0:MacroStartPos] + os.path.normpath(cls.join(cls.WORKSPACE, Substr))
+                        if Substr.endswith(os.sep):
+                            Final = os.sep
+                        else:
+                            Final = ''
+                        PathList[i] = str[0:MacroStartPos] + os.path.normpath(cls.join(cls.WORKSPACE, Substr)) + Final
             PathStr = ' '.join(PathList)
         return PathStr
     