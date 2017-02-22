c13669071f2c700586e8ed673495a303933ae39a
Laszlo Ersek <lersek@redhat.com>
2017-02-22 10:34:48 +0100

    OvmfPkg/QemuFwCfgS3Lib: implement opcode APIs for Base Null instance
    In the Base Null instance:
    
    - QemuFwCfgS3Enabled() returns constant FALSE. This is unique to the Base
      Null instance, and the function is already present in
      "QemuFwCfgS3Base.c".
    
    - The QemuFwCfgS3TransferOwnership() function must never be called
      (according to the documentation, given the above). This is also unique
      to the Base Null instance, so implement the function in
      "QemuFwCfgS3Base.c".
    
    - Consequently, the QemuFwCfgS3WriteBytes(), QemuFwCfgS3ReadBytes(),
      QemuFwCfgS3SkipBytes(), and QemuFwCfgS3CheckValue() functions must never
      be called either. This behavior is not unique to the Base Null instance
      (it will be shared with the PEI fw_cfg instance), so add these functions
      to "QemuFwCfgS3BasePei.c".
    
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=394
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>

--- OvmfPkg/Library/QemuFwCfgS3Lib/QemuFwCfgS3Base.c
+++ OvmfPkg/Library/QemuFwCfgS3Lib/QemuFwCfgS3Base.c
@@ -16,6 +16,7 @@
   WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 **/
 
+#include <Library/DebugLib.h>
 #include <Library/QemuFwCfgS3Lib.h>
 
 /**
@@ -36,4 +37,73 @@ QemuFwCfgS3Enabled (
   )
 {
   return FALSE;
+}
+
+
+/**
+  Install the client module's FW_CFG_BOOT_SCRIPT_APPEND_FUNCTION callback for
+  when the production of ACPI S3 Boot Script opcodes becomes possible.
+
+  Take ownership of the client-provided Context, and pass it to the callback
+  function, when the latter is invoked.
+
+  Allocate scratch space for those ACPI S3 Boot Script opcodes to work upon
+  that the client will produce in the callback function.
+
+  @param[in] Append             FW_CFG_BOOT_SCRIPT_APPEND_FUNCTION to invoke
+                                when the production of ACPI S3 Boot Script
+                                opcodes becomes possible. Append() may be
+                                called immediately from
+                                QemuFwCfgS3TransferOwnership().
+
+  @param[in,out] Context        Client-provided data structure for the Append()
+                                callback function to consume.
+
+                                If Context points to dynamically allocated
+                                memory, then Append() must release it.
+
+                                If Context points to dynamically allocated
+                                memory, and QemuFwCfgS3TransferOwnership()
+                                returns successfully, then the caller of
+                                QemuFwCfgS3TransferOwnership() must neither
+                                dereference nor even evaluate Context any
+                                longer, as ownership of the referenced area has
+                                been transferred to Append().
+
+  @param[in] ScratchBufferSize  The size of the scratch buffer that will hold,
+                                in reserved memory, all client data read,
+                                written, and checked by the ACPI S3 Boot Script
+                                opcodes produced by Append().
+
+  @retval RETURN_UNSUPPORTED       The library instance does not support this
+                                   function.
+
+  @retval RETURN_NOT_FOUND         The fw_cfg DMA interface to QEMU is
+                                   unavailable.
+
+  @retval RETURN_BAD_BUFFER_SIZE   ScratchBufferSize is too large.
+
+  @retval RETURN_OUT_OF_RESOURCES  Memory allocation failed.
+
+  @retval RETURN_SUCCESS           Append() has been installed, and the
+                                   ownership of Context has been transferred.
+                                   Reserved memory has been allocated for the
+                                   scratch buffer.
+
+                                   A successful invocation of
+                                   QemuFwCfgS3TransferOwnership() cannot be
+                                   rolled back.
+
+  @return                          Error codes from underlying functions.
+**/
+EFIAPI
+RETURN_STATUS
+QemuFwCfgS3TransferOwnership (
+  IN     FW_CFG_BOOT_SCRIPT_APPEND_FUNCTION *Append,
+  IN OUT VOID                               *Context,          OPTIONAL
+  IN     UINTN                              ScratchBufferSize
+  )
+{
+  ASSERT (FALSE);
+  return RETURN_UNSUPPORTED;
 }
